25898673ef34f43d249c8f0ec9ee51c46ff069e2
mateusz.markowski <mateusz.markowski@524f2690-4752-0410-8233-2f00ced2aefc>
2009-05-06 23:36:20 +0000

    Zmiana reprezentacji vizualizacji w bazie danych

--- AcornWSClient/src/acornwsclient/download/DBDataDownloader.java
+++ AcornWSClient/src/acornwsclient/download/DBDataDownloader.java
@@ -17,6 +17,7 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.dbStructs.NameStruct;
+import org.visualapi.VisEdge;
 
 /**
  *
@@ -81,11 +82,72 @@ public class DBDataDownloader {
         }
     }
 
-    public List<String> getAllVisualizationNames(){
+    public List<String> getAllVisualizationNames() {
         return port.getAllVisualizationNames();
     }
 
-    public boolean removeVisualization(String name){
+    public boolean removeVisualization(String name) {
         return port.removeVisualization(name);
+    }
+
+    public List<String> getDescendantVisualizationNames(String modelName) {
+        return port.getDescendantVisualizationNames(modelName);
+    }
+
+    private List<String> getDeserializedString(String inputString) {
+        byte[] byteArray = null;
+        try {
+            byteArray = inputString.getBytes(webServiceEncoding);
+        } catch (UnsupportedEncodingException ex) {
+            Logger.getLogger(DBDataDownloader.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        ByteArrayInputStream stream = new ByteArrayInputStream(byteArray);
+
+        List<String> strings = new ArrayList<String>(0);
+        XMLDecoder d = new XMLDecoder(new BufferedInputStream(stream));
+
+        try {
+            while (true) {
+                strings.add((String) d.readObject());
+            }
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            return strings;
+        }
+    }
+
+    /**
+     *
+     * @param visualizationName visualization name which is downloaded from DataBase
+     * @return visualization as list of edges containing visPlace and VisTransition as source/target of edge
+     */
+    public List<VisEdge> getVisualization(String visualizationName) {
+        String serializedString = port.getVisualization(visualizationName);
+        List<VisEdge> edges = getDeserializedVisEdge(serializedString);
+        for (VisEdge edge : edges) {
+            edge.getSource().removeNullsFromSets();
+            edge.getTarget().removeNullsFromSets();
+        }
+        return edges;
+    }
+
+    private List<VisEdge> getDeserializedVisEdge(String inputString) {
+        byte[] byteArray = null;
+        try {
+            byteArray = inputString.getBytes(webServiceEncoding);
+        } catch (UnsupportedEncodingException ex) {
+            Logger.getLogger(DBDataDownloader.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        ByteArrayInputStream stream = new ByteArrayInputStream(byteArray);
+
+        List<VisEdge> edges = new ArrayList<VisEdge>(0);
+        XMLDecoder d = new XMLDecoder(new BufferedInputStream(stream));
+
+        try {
+            while (true) {
+                edges.add((VisEdge) d.readObject());
+            }
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            return edges;
+        }
     }
 }

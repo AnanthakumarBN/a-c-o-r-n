098ae8452888b8ac4ea6760fda84cd111d618482
jacek.sroka <jacek.sroka@524f2690-4752-0410-8233-2f00ced2aefc>
2009-09-01 10:00:29 +0000

    updating the ant build script

--- trunk/acorn-db/src/acorn/db/EVisPlace.java
+++ trunk/acorn-db/src/acorn/db/EVisPlace.java
@@ -6,7 +6,11 @@ package acorn.db;
 
 import java.awt.Point;
 import java.io.Serializable;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
@@ -19,6 +23,11 @@ import javax.persistence.NamedQueries;
 @NamedQueries({
 })
 public class EVisPlace extends EVisNode implements Serializable {
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    @Column(name = "ID", nullable = false)
+    Long id;
 
 //    private static final long serialVersionUID = 1L;
     @ManyToOne
--- trunk/acorn-db/src/acorn/db/EVisTransition.java
+++ trunk/acorn-db/src/acorn/db/EVisTransition.java
@@ -6,7 +6,11 @@ package acorn.db;
 
 import java.awt.Point;
 import java.io.Serializable;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
@@ -16,6 +20,11 @@ import javax.persistence.ManyToOne;
  */
 @Entity
 public class EVisTransition extends EVisNode implements Serializable {
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    @Column(name = "ID", nullable = false)
+    Long id;
 
     
     @ManyToOne

a7a9c8f401517d385b9c2dbce8ee68bbc587d8d4
jacek.sroka <jacek.sroka@524f2690-4752-0410-8233-2f00ced2aefc>
2009-10-30 16:00:12 +0000

    new style of captcha

--- acorn-0.3/src/java/acorn/userManagement/CaptchaServiceSingleton.java
+++ acorn-0.3/src/java/acorn/userManagement/CaptchaServiceSingleton.java
@@ -1,7 +1,9 @@
 
 package acorn.userManagement;
+import com.octo.captcha.engine.image.gimpy.SimpleListImageCaptchaEngine;
 import com.octo.captcha.service.image.ImageCaptchaService;
 import com.octo.captcha.service.image.DefaultManageableImageCaptchaService;
+import com.octo.captcha.service.captchastore.FastHashMapCaptchaStore;
 
 /**
  * This file has been copied from JCaptcha tutorial
@@ -11,7 +13,10 @@ import com.octo.captcha.service.image.DefaultManageableImageCaptchaService;
 
 public class CaptchaServiceSingleton {
     
-    private static ImageCaptchaService instance = new DefaultManageableImageCaptchaService();
+    //private static ImageCaptchaService instance = new DefaultManageableImageCaptchaService();
+
+    //private static ImageCaptchaService instance = new DefaultManageableImageCaptchaService(new FastHashMapCaptchaStore(), new BasicListGimpyEngine(), 180, 100000, 75000);
+    private static ImageCaptchaService instance = new DefaultManageableImageCaptchaService(new FastHashMapCaptchaStore(), new SimpleListImageCaptchaEngine(), 180, 100000, 75000);
     
     public static ImageCaptchaService getInstance(){
         return instance;

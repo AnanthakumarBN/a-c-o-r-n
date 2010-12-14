97202ae849702fbd8ab1b502a9ddc41d0d9dff75
jakub.lacki <jakub.lacki@gmail.com>
2010-12-14 22:11:55 +0000

    new testcase: add and remove a model

--- acorn-0.3/selenium/WebUserManagement.java
+++ acorn-0.3/selenium/WebUserManagement.java
@@ -41,12 +41,12 @@ public class WebUserManagement {
 
     public static void logoutUser(Selenium selenium, SeleneseTestCase testCase) {
         testCase.verifyTrue(selenium.isTextPresent("Log Out"));
-        selenium.click("//form[@id='menu:j_id_id189pc3']/a/em");
+        selenium.click("//div[@id='menu']/ul/li[4]/a/em");
         selenium.waitForPageToLoad("30000");
         selenium.open("/acorn/register.jsf");
     }
 
-    private static void loginAdmin(Selenium selenium, SeleneseTestCase testCase) {
+    public static void loginAdmin(Selenium selenium, SeleneseTestCase testCase) {
         selenium.open("/acorn/login.jsf");
         selenium.type("content:login:login", LOGIN_ROOT);
         String passwdRoot = "rosomak";
